b194f430803dee5c051ace43167c524d2a27a4f9
Victor Alberto Romero <vialrogo@gmail.com>
2013-12-14 20:18:40 -0200

    Vamos por newtongit status

--- EP2/EP2.f90
+++ EP2/EP2.f90
@@ -2,49 +2,50 @@
 !*****************************************************************************
 program main
 
+    ! Module used
+    use mgh_eval
+
     implicit none
 
+    ! Problems parameters
+    integer,parameter::  n = 2
+    integer,parameter::  p = 14
+    integer :: prob
+
     ! Variables
-    integer, parameter :: n=2
-    real*8  :: xsol(n), xini(n)
-
-    ! Set initial point
-    xini(1) = 1.2d0
-    xini(2) = 1.2d0
-
-    ! Call the function
-    call linearSearch(n,xini,xsol)
-
-    ! Print solution
-    print *, 'The solutin is: ', xsol(1), xsol(2) 
+    real*8  :: x(n)
+    
+    do prob=p, p, 1
+        
+        call initpt(n, x, prob, 1.0d0) ! Set initial point
+        call linearSearch(n,x,prob) ! Call the subrutine
+    
+    end do
 
 end program main
     
 !*****************************************************************************
 !*****************************************************************************
-subroutine linearSearch(n,xini,xnew)
+subroutine linearSearch(n,x,prob)
  
-    ! Input arguments
+    ! Arguments
     integer, intent(in) :: n
-    real*8, intent(in) :: xini(n)
+    integer, intent(in) :: prob
+    real*8, intent(inout) :: x(n)
 
-    ! Output arguments
-    real*8,  intent(out) :: xnew(n)
-    
     ! Parameters
     integer, parameter :: nmax  = 1000
     real*8,  parameter :: gamma = 1.0d-04
     real*8,  parameter :: epsg  = 1.0d-010
 
     ! Variables
-    integer :: iter
+    integer :: iter,i
     real*8  :: alpha,f,fnew,gnorm,gtd
-    real*8  :: g(n),d(n),x(n)
+    real*8  :: g(n),d(n),xnew(n)
 
     ! Eval function
-    x = xini
-    call evalf(n,x,f)
-    call evalg(n,x,g)
+    call evalf(n,x,f,prob)
+    call evalg(n,x,g,prob)
 
     ! Calculate gradient norm
     gnorm = maxval(abs(g))
@@ -53,12 +54,12 @@ subroutine linearSearch(n,xini,xnew)
     do while ( gnorm >= epsg )
 
         ! Calculate a new direction
-        d = -g 
+        call calculateDMaxDesc(n,x,d,prob) 
 
         ! Calculate a new point
         alpha = 1.0d0
         xnew = x + alpha * d
-        call evalf(n,xnew,fnew)
+        call evalf(n,xnew,fnew,prob)
 
         ! Confirm if it's a usefull point
         gtd = dot_product(g,d)
@@ -66,51 +67,106 @@ subroutine linearSearch(n,xini,xnew)
             
             alpha = 0.5d0 * alpha
             xnew = x + alpha * d
-            call evalf(n,xnew,fnew)
+            call evalf(n,xnew,fnew,prob)
         
         end do
 
         ! Update the point
         f = fnew
         x = xnew
-        call evalg(n,x,g)
+        call evalg(n,x,g,prob)
         gnorm = maxval(abs(g))
         
         ! Output
         iter = iter + 1
-        write(*,"('iter: ' I8 '  f: ',E14.6,'  gnorm: ',E14.6)") iter, f ,gnorm
 
     end do
+
+    ! Print final conditions
+    write(*,*) 'Iterations:', iter, 'f:', f
+    write(*,*) 'The solution is: ', x 
    
 end subroutine linearSearch
 
 !*****************************************************************************
 !*****************************************************************************
-subroutine evalf(n,x,f)
+subroutine calculateDMaxDesc(n,x,d,prob)
 
-    ! Input arguments
+    use mgh_eval
+
+    ! Arguments
     integer, intent(in) :: n
+    integer, intent(in) :: prob
     real*8, intent(in) :: x(n)
-    
-    ! Output arguments
+    real*8, intent(out) :: d(n)
+
+    call grdfcn(n, x, d, prob)
+    d = -d
+
+end subroutine calculateDMaxDesc
+
+!*****************************************************************************
+!*****************************************************************************
+subroutine calculateDNewton(n,x,d,prob)
+
+    use mgh_eval
+
+    ! Arguments
+    integer, intent(in) :: n
+    integer, intent(in) :: prob
+    real*8, intent(in) :: x(n)
+    real*8, intent(out) :: d(n)
+
+    call grdfcn(n, x, d, prob)
+    d = -d
+
+end subroutine calculateDNewton
+
+!*****************************************************************************
+!*****************************************************************************
+subroutine evalf(n,x,f,prob)
+
+    use mgh_eval
+
+    ! Arguments
+    integer, intent(in) :: n
+    integer, intent(in) :: prob
+    real*8, intent(in) :: x(n)
     real*8, intent(out) :: f
 
-    f = 100.0d0 * ( x(2) - x(1) ** 2 ) ** 2 + ( 1.0d0 - x(1) ) ** 2
+    call objfcn(n, x, f, prob)
 
 end subroutine evalf
 
 !*****************************************************************************
 !*****************************************************************************
-subroutine evalg(n,x,g)
+subroutine evalg(n,x,g,prob)
 
-    ! Input arguments
+    use mgh_eval
+    
+    ! Arguments
     integer, intent(in) :: n
+    integer, intent(in) :: prob
     real*8,  intent(in) :: x(n)
-
-    ! Output arguments
     real*8,  intent(out) :: g(n)
 
-    g(1) = - 400.0d0 * ( x(2) - x(1) ** 2 ) * x(1) - 2.0d0 * ( 1.0d0 - x(1) )
-    g(2) =   200.0d0 * ( x(2) - x(1) ** 2 )
+    call grdfcn(n, x, g, prob)
 
 end subroutine evalg
+
+!*****************************************************************************
+!*****************************************************************************
+subroutine evalh(n,x,hd,hod,prob)
+
+    use mgh_eval
+    
+    ! Arguments
+    integer, intent(in) :: n
+    integer, intent(in) :: prob
+    real*8,  intent(in) :: x(n)
+    real*8,  intent(out) :: hd(n) 
+    real*8,  intent(out) :: hod( (n*n -n)/2 )
+    
+    call hesfcn(n, x, hd, hod, prob)
+
+end subroutine evalh
